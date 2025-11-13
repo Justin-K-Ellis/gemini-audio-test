393814682ccea65167c1f45d257134202b9e5c90
Justin-K-Ellis <jlke45@gmail.com>
2025-11-13 14:53:42 +0900

    refactor: note create_dialog_prompt func return type as str

--- lib/createdialogprompt.py
+++ lib/createdialogprompt.py
@@ -1,4 +1,4 @@
-def create_dialog_prompt(dialog_list: list[dict[str, str]]):
+def create_dialog_prompt(dialog_list: list[dict[str, str]]) -> str:
     result = f"TTS the following conversation between {dialog_list[0]["speaker"]} and {dialog_list[1]["speaker"]}:"
     for part in dialog_list:
         turn = f"\n{part["speaker"]}: {part["text"]}"
