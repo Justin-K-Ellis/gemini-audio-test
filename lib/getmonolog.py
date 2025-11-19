bb694cbe6692cd296f8f198098d0479ba63f848d
Justin-K-Ellis <jlke45@gmail.com>
2025-11-19 14:27:25 +0900

    feat: loop thru & generate monolog audio
    Unlike the dialogs, there doesn't seem to be an issue with voice roles (as expected,
    but you never know with AI).

--- lib/getmonolog.py
+++ lib/getmonolog.py
@@ -1,24 +1,27 @@
 from google import genai
 from google.genai import types
-
 from .wavefile import wave_file
 
-def get_monolog(prompt: str):
+from utils.Monolog import Monolog
+
+def get_monolog(monolog: Monolog, index: int):
     """Send prompt to Gemini api and save audio to current directory.
     Creates a monolog."""
     client = genai.Client()
 
-    print("Creating the monolog...")
+    speaker = "Kore" if monolog.speaker_gender == "female" else "Orus"
+
+    print(f"Creating monolog {index}...")
     try:
         response = client.models.generate_content(
         model="gemini-2.5-flash-preview-tts",
-        contents=prompt,
+        contents=monolog.text,
         config=types.GenerateContentConfig(
             response_modalities=["AUDIO"],
             speech_config=types.SpeechConfig(
                 voice_config=types.VoiceConfig(
                     prebuilt_voice_config=types.PrebuiltVoiceConfig(
-                    voice_name='Kore',
+                    voice_name=speaker,
                     )
                 )
             ),
@@ -27,9 +30,10 @@ def get_monolog(prompt: str):
 
         data = response.candidates[0].content.parts[0].inline_data.data
 
-        file_name='monolog_test2.wav'
-        wave_file(file_name, data) # Saves the file to current directory
-        print("Monolog created and saved!")
+        file_name=f'monolog_{index}.wav'
+        wave_file(f"./output/{file_name}", data)
+        print(f"Monolog {index} created and saved!")
     except Exception as error:
-        print("Something went wrong when creating the monolog.")
+        print(f"Something went wrong when creating monolog {index}.\n")
+        print(monolog.text + "\n")
         print(error)
--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from lib.createdialogprompt import create_dialog_prompt
 from lib.getmonolog import get_monolog
 from lib.getdialog import get_dialog
 from utils.Dialog import Dialog
+from utils.Monolog import Monolog
 
 load_dotenv()
 
+# == Dialogs ==
 dialog_text1 = """
 Man: What do you want to have for dinner today? Pasta?
 Woman: How about curry? 
@@ -24,10 +26,27 @@ dialog_texts: list[Dialog] = [
     Dialog(dialog_text2, speaker1="Tom", speaker1_gen="male", speaker2="Lisa", speaker2_gen="female")
 ]
 
+# == Monologs ==
+mono1 = """
+Welcome to Green Market. You can find fresh apples near the entrance. Today, we have 
+fresh eggs. They are on sale. Don't miss our special bread in asile four.
+"""
+
+mono2 = """
+There is a beautiful mountain in my town. There is a waterfall near the bottom of the 
+mountain and a small shrine at the top of the mountain.
+"""
+
+monolog_texts: list[Monolog] = [
+    Monolog(text=mono1, speaker_gender="female"),
+    Monolog(text=mono2, speaker_gender="male")
+]
 
 def main():
-    for index, dialog in enumerate(dialog_texts):
-        get_dialog(dialog, index=index)
+    # for index, dialog in enumerate(dialog_texts):
+    #     get_dialog(dialog, index=index)
+    for index, monolog in enumerate(monolog_texts):
+        get_monolog(monolog=monolog, index=index)
 
 
 if __name__ == "__main__":
