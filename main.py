e2a126759629b75ea021c398d8b5d7361f0caa56
Justin-K-Ellis <jlke45@gmail.com>
2025-11-19 15:38:56 +0900

    feat: generate monolog audio from csv text

--- main.py
+++ main.py
@@ -1,51 +1,36 @@
 from dotenv import load_dotenv
 
-from lib.createdialogprompt import create_dialog_prompt
 from lib.getmonolog import get_monolog
 from lib.getdialog import get_dialog
+from lib.read_monolog_csv import read_in_monolog_csv
 from utils.Dialog import Dialog
 from utils.Monolog import Monolog
+
 
 load_dotenv()
 
 # == Dialogs ==
-dialog_text1 = """
-Man: What do you want to have for dinner today? Pasta?
-Woman: How about curry? 
-Man: I had that for lunch. 
-"""
+# dialog_text1 = """
+# Man: What do you want to have for dinner today? Pasta?
+# Woman: How about curry? 
+# Man: I had that for lunch. 
+# """
 
-dialog_text2 = """
-Tom: Look! It's snowing a lot today!
-Lisa: Yeah! Let's make a snowman outside.
-Tom: Good idea. After that, we can drink hot chocolate inside.
-"""
+# dialog_text2 = """
+# Tom: Look! It's snowing a lot today!
+# Lisa: Yeah! Let's make a snowman outside.
+# Tom: Good idea. After that, we can drink hot chocolate inside.
+# """
 
-dialog_texts: list[Dialog] = [
-    Dialog(dialog_text1, speaker1="Man", speaker1_gen="male", speaker2="Woman", speaker2_gen="female"),
-    Dialog(dialog_text2, speaker1="Tom", speaker1_gen="male", speaker2="Lisa", speaker2_gen="female")
-]
-
-# == Monologs ==
-mono1 = """
-Welcome to Green Market. You can find fresh apples near the entrance. Today, we have 
-fresh eggs. They are on sale. Don't miss our special bread in asile four.
-"""
-
-mono2 = """
-There is a beautiful mountain in my town. There is a waterfall near the bottom of the 
-mountain and a small shrine at the top of the mountain.
-"""
-
-monolog_texts: list[Monolog] = [
-    Monolog(text=mono1, speaker_gender="female"),
-    Monolog(text=mono2, speaker_gender="male")
-]
+# dialog_texts: list[Dialog] = [
+#     Dialog(dialog_text1, speaker1="Man", speaker1_gen="male", speaker2="Woman", speaker2_gen="female"),
+#     Dialog(dialog_text2, speaker1="Tom", speaker1_gen="male", speaker2="Lisa", speaker2_gen="female")
+# ]
 
 def main():
-    # for index, dialog in enumerate(dialog_texts):
-    #     get_dialog(dialog, index=index)
-    for index, monolog in enumerate(monolog_texts):
+    monologs_path = "./input/monologs.csv"
+    monologs = read_in_monolog_csv(filepath=monologs_path)
+    for index, monolog in enumerate(monologs):
         get_monolog(monolog=monolog, index=index)
 
 
